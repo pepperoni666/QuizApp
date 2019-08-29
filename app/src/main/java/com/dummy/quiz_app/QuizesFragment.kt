9773858a7fc6c805189533e392e5b4c84fa95133
Oleksii.Perets <oleksii.perets@asseco.pl>
2019-08-29 23:32:01 +0200

    Fragments prepared

--- app/src/main/java/com/dummy/quiz_app/QuizesFragment.kt
+++ app/src/main/java/com/dummy/quiz_app/QuizesFragment.kt
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.mvrx.BaseMvRxFragment
 
-class QuizesFragment : Fragment() {
+class QuizesFragment : BaseMvRxFragment() {
+    override fun invalidate() {
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,

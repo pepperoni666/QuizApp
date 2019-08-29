71999d4a1ef2e4008dd24bb8c61c75705675973f
Oleksii.Perets <oleksii.perets@asseco.pl>
2019-08-29 23:22:46 +0200

    Migrated to AndroidX + navigation graph added

--- app/src/main/java/com/dummy/quiz_app/MainActivity.kt
+++ app/src/main/java/com/dummy/quiz_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.dummy.quiz_app
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/dummy/quiz_app/SolvingFragment.kt
@@ -0,0 +1,20 @@
+package com.dummy.quiz_app
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SolvingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_solving, container, false)
+    }
+}
